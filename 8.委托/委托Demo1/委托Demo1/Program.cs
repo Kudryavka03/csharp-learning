4d4669e7c1a0cbe36fa0d29dfdb71111111b3900
otaku <2512975065@qq.com>
2023-04-29 23:43:03 +0800

    Init 委托

---
+++ 8.委托/委托Demo1/委托Demo1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 委托Demo1
+{
+
+    //委托其实是个引用类型 说白点他是个类，保存了方法的指针
+    //好了，委托学完了
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
